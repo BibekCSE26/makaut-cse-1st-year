e46a67b63b1cdd8f2568933c51f64b70c2f57d0b
Bibek Mondal <bibekmondal255@gmail.com>
2026-07-14 20:39:11 +0530

    Delete 01-hello-world.c

--- 01-hello-world.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main() {
- printf("Hello Makaut\n");
- printf("Name: Bibek Mondal\n");
- printf("Stream: CSE 1st Year\n");
- printf("Goal: 50+ C Programs for Placement\n");
- return 0;
-}
